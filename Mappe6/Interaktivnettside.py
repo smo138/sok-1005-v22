15264171f56d8a5778211c7431243e6a16a74df5
smo138 <89776885+smo138@users.noreply.github.com>
2022-04-08 09:30:35 +0200

    Update Interaktivnettside.py

--- Mappe6/Interaktivnettside.py
+++ Mappe6/Interaktivnettside.py
@@ -6,4 +6,6 @@ select("Velg frukt", ['Melon', 'Tomat'])
 checkbox("Går det bra?", options=["Jævla bra."])
 radio("Hva liker du å gjøre?", options=['Eta', 'Sova', 'Skule'])
 
+#inspo fra nette :) 
+
 
